753f5364014419ae38fc842d677070a4cf41289b
Josh Bartow <jbartow92@gmail.com>
2023-11-20 11:38:11 -0600

    fixed the navbar

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -33,7 +33,11 @@ export const NavBar = ({ token, setToken }) => {
           {
             token
               ?
-              <Link to="/" className="navbar-item">Posts</Link>
+              <>
+              <Link to="/" className="navbar-item">Home</Link>
+              <Link to="/posts" className="navbar-item">Posts</Link>
+              <Link to="/categories" className="navbar-item">Category Manager</Link>
+              </>
               :
               ""
           }
