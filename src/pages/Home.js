d7ac1fb0c168bfc6ed9d70ff11c6d76909e36785
NathanielMcfarlin <mcfarlinnate@gmail.com>
2023-11-27 13:29:48 -0600

    Fixed home page and removed rare user

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 export const Home = () => {
     return (
       <main className='text-slate-900 pl-10 pr-10'>
-        <h1 className='text-4xl'>Welcome to Extra Medium</h1>
+        <h1 className='text-4xl'>Welcome to Rare!</h1>
       </main>
     )
   }
