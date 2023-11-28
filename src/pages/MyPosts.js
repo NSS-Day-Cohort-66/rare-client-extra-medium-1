295f9f7be416ae3024bdb9a614c4fbc3985e65f8
Michelle Totherow <tym4me13@gmail.com>
2023-11-27 19:32:10 -0600

    refactor to delete a post

--- src/pages/MyPosts.js
+++ src/pages/MyPosts.js
@@ -2,46 +2,57 @@ import { useEffect, useState } from "react";
 import "./pages.css";
 import { getAllPosts, deletePost } from "../services/postServices";
 
+
 export const MyPosts = ({ setToken, token }) => {
   const [myPosts, setMyPosts] = useState([]);
   const [postToDelete, setPostToDelete] = useState(null);
  
-  const getAndSetMyPosts = () => {
-    getAllPosts().then((postsArray) => {
+  const getAndSetMyPosts = async () => {
+    try {
+      const postsArray = await getAllPosts();
       const filteredArray = postsArray.filter((post) => post.is_owner === true);
       const sortedArray = filteredArray.sort((a, b) => {
         return new Date(b.publication_date) - new Date(a.publication_date);
       });
       setMyPosts(sortedArray);
-    });
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    }
   };
 
   const handleDeleteClick = (post) => {
     setPostToDelete(post);
+  };
 
-    const confirmDelete = window.confirm("Are you sure you want to delete this post?");
-    if (confirmDelete) {
+  useEffect(() => {
+    if (postToDelete) {
+      const handleDeleteConfirmation = async () => {
+        const confirmDelete = window.confirm("Are you sure you want to delete this post?");
+
+        if (confirmDelete) {
+          try {
+            await deletePost(postToDelete.id);
+
+            // Update the state immediately after successful deletion
+            setMyPosts((prevPosts) =>
+              prevPosts.filter((post) => post.id !== postToDelete.id)
+            );
+          } catch (error) {
+            console.error("Error deleting post:", error);
+          } finally {
+            setPostToDelete(null);
+          }
+        }
+      };
+
       handleDeleteConfirmation();
     }
-  };
+  }, [postToDelete]);
 
-  const handleDeleteConfirmation = () => {
-    if (postToDelete) {
-      deletePost(postToDelete.id)
-        .then(() => {
-          setPostToDelete(null);
-          getAndSetMyPosts();
-        })
-        .catch((error) => {
-          console.error("Error deleting post:", error);
-        });
-    }
-  };
-
-  
   useEffect(() => {
     getAndSetMyPosts();
-  }, []);
+  }, [postToDelete]); // Fetch posts whenever postToDelete changes
+
 
   return (
     <>
